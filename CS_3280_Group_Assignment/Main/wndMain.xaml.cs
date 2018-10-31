c16aef61f45171b5c37b4807a8b335567b70d53c
DexterSnyder <40254136+DexterSnyder@users.noreply.github.com>
2018-10-31 16:09:28 -0600

    Stubbed out main window

--- CS_3280_Group_Assignment/Main/wndMain.xaml.cs
+++ CS_3280_Group_Assignment/Main/wndMain.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,207 @@ namespace CS_3280_Group_Assignment.Main
     /// </summary>
     public partial class wndMain : Window
     {
+        /// <summary>
+        /// If the user is editing an invoice
+        /// </summary>
+        private bool isEditing;
+
+        /// <summary>
+        /// If the user is adding an invoice
+        /// </summary>
+        private bool isAdding;
+
         public wndMain()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+                loadInvoices();
+                isEditing = false;
+                isAdding = false;
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
-    }
-}
+
+        /// <summary>
+        /// Loads the invoices into the list box
+        /// </summary>
+        private void loadInvoices ()
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void refreshInvoices ()
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+       
+        /// <summary>
+        /// Allows the user to edit the selected invoice
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                isEditing = true;
+                //get selection
+                //unlock text boxes
+                refreshInvoices();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Delete button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //get selection
+                //delete selection
+                refreshInvoices();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// New invoice button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                isAdding = true;
+                //unlock textboxes
+                //set invoice number
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Save button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //get the data
+                if (isAdding)
+                {
+                    //send SQL
+                    isAdding = false;
+                    refreshInvoices();
+                }
+                if(isEditing)
+                {
+                    //send SQL
+                    isEditing = false;
+                    refreshInvoices();
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Update button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //open the update form
+        }
+
+        /// <summary>
+        /// Search button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            //open the search form
+        }
+
+        /// <summary>
+        /// Close out application, since this is the main window of the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                Application.Current.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+        /// <summary>
+        /// Handle the error.
+        /// </summary>
+        /// <param name="sClass">The class in which the error occurred in.</param>
+        /// <param name="sMethod">The method in which the error occurred in.</param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                //Would write to a file or database here.
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                             "HandleError Exception: " + ex.Message);
+            }
+        }
+
+    }//class
+}//namespace
